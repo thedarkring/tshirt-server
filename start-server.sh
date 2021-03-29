4658b496c2e9483e774d980547bcbd8cbca13811
bryanclauss <45980672+bryanclauss@users.noreply.github.com>
2021-03-29 16:10:20 -0400

    Update start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -2,9 +2,10 @@
 # version 2
 PATH=/home/pi/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
 cd /home/pi/tshirt/tshirt-server/
+git reset --hard HEAD
 git pull
 . /home/pi/tshirt/bin/activate
-mv /home/pi/tshirt/tshirt-server/start-server.sh /home/pi/start-server.sh
+cp /home/pi/tshirt/tshirt-server/start-server.sh /home/pi/start-server.sh
 chmod +x /home/pi/tshirt/tshirt-server/listen_button.py
 nohup python3 /home/pi/tshirt/tshirt-server/listen_button.py &
 python /home/pi/tshirt/tshirt-server/tshirt/manage.py runserver 0.0.0.0:8080
